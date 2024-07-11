e1a062279d6ba37ddca6dcedefded09dc530c495
VyacheslavLikhosherst <slavaknyazev2003@gmail.com>
2024-07-11 23:12:26 +0500

    The functionality for editing tasks has been improved, and the functionality for deleting tasks has been added.

--- todo-list.py
+++ todo-list.py
@@ -42,11 +42,25 @@ def get_tasks(db):
     rows = cursor.fetchall()
     return rows
 
+def get_one_task(db, number):
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM todolist WHERE id = ?', (number,))
+    rows = cursor.fetchall()
+    if rows:
+        print(f"{rows[0][0]}. Тема: {rows[0][1]}. Описание задачи: {rows[0][2]}")
+    else:
+        print("Задача с таким номером не найдена")
+        return False
+    cursor.close()
+
 def print_tasks(db):
     rows = get_tasks(db)
     print("Ваш список задач:")
-    for row in rows:
-        print(f"{row[0]}. Тема: {row[1]}. Описание задачи: {row[2]}")
+    if rows:
+        for row in rows:
+            print(f"{row[0]}. Тема: {row[1]}. Описание задачи: {row[2]}")
+    else:
+        print("Ваш список задач пуст.")
 
 
 #Функции обновления существующих задач
@@ -74,34 +88,125 @@ def update_theme_and_description(db, number_task, theme, description):
             SET name = ?, description = ? 
             WHERE id = ?
         ''', (theme, description, number_task))
-#////////////////////////////////////////
-
 
 def edit_task(db):
     while True:
         try:
             number_task = input("Введите номер задачи, которую Вы хотите отредактировать: ").strip()
             number = int(number_task)
+            result_func = get_one_task(db, number)
+            if result_func == False:
+                continue
             while True:
                 print("Что вы хотите отредактировать?")
                 print("-------------------")
                 print("1. Тему задачи")
                 print("2. Описание задачи")
                 print("3. Тему и описание")
+                print("4. Отмена")
                 print("-------------------")
                 try:
                     operation = input("Выберите действие: ").strip()
                     operation = int(operation)
-                    if operation < 1 or operation > 3:
-                        print("Введите корректное значение в виде цифры от 1 до 3")
-                    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!     
-                    # Начиная с этого участка кода, надо закончить функционал 
-                    # #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!     
+                    if operation < 1 or operation > 4:
+                        print("Введите корректное значение в виде цифры от 1 до 4") 
+                    elif operation == 1:
+                        new_theme = input("Введите новое название для Вашей задачи: ")
+                        update_theme(db, number, new_theme)
+                        print("Изменения успешно сохранены.")
+                        get_one_task(db, number)
+                    elif operation == 2:
+                        new_description = input("Введите новое описание для Вашей задачи: ")
+                        update_description(db, number, new_description)
+                        print("Изменения успешно сохранены.")
+                        get_one_task(db, number)
+                    elif operation == 3:
+                        new_theme = input("Введите новое название для Вашей задачи: ")
+                        new_description = input("Введите новое описание для Вашей задачи: ")
+                        update_theme_and_description(db, number, new_theme, new_description)
+                        print("Изменения успешно сохранены.")
+                        get_one_task(db, number)
+                    else:
+                        print("Отмена изменений...")
+                        break
                 except TypeError:
                     print("Вам необходимо ввести число без лишних символов.")
+            break
                 
         except TypeError:
             print("Вам необходимо ввести число без лишних символов.")
+#////////////////////////////////////////
+def delete_all_tasks(db):
+    with db:
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM todolist')
+        rows = cursor.fetchall()
+        if rows:
+            #print(f"{rows[0][0]}. Тема: {rows[0][1]}. Описание задачи: {rows[0][2]}")
+            db.execute('DELETE FROM todolist')
+            reset_ids(db)
+            print("Задачи успешно удалены.")
+        else:
+            print("Список задач уже пуст.")
+
+def delete_one_task(db, number):
+    with db:
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM todolist WHERE id = ?', (number,))
+        rows = cursor.fetchall()
+        if rows:
+            #print(f"{rows[0][0]}. Тема: {rows[0][1]}. Описание задачи: {rows[0][2]}")
+            db.execute('DELETE FROM todolist WHERE id = ?', (number,))
+            reset_ids(db)
+            print(f"Задача №{number} успешно удалена.")
+        else:
+            print("Задачи с таким номером не существует")
+            return False
+
+def delete_tasks(db):
+    while True:
+        print("-------------------")
+        print("1. Удалить задачу")
+        print("2. Удалить все задачи")
+        print("3. Отмена")
+        print("-------------------")
+        try:
+            operation = input("Выберите действие: ").strip()
+            operation = int(operation)
+            if operation < 1 or operation > 3:
+                print("Введите корректное значение в виде цифры от 1 до 3") 
+            elif operation == 1:
+                number_task = input("Введите номер задачи, которую Вы хотите удалить: ").strip()
+                number = int(number_task)
+                result_func = delete_one_task(db, number)
+                if result_func == False:
+                    continue
+                else:
+                    break
+            elif operation == 2:
+                delete_all_tasks(db)
+                break
+            else:
+                print("Отмена изменений...")
+                break
+        except TypeError:
+            print("Вам необходимо ввести число без лишних символов.")
+
+def reset_ids(db):
+    with db:
+        db.execute('''
+            CREATE TABLE IF NOT EXISTS todolist_temp (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                description TEXT NOT NULL
+            )
+        ''')
+        db.execute('''
+            INSERT INTO todolist_temp (name, description)
+            SELECT name, description FROM todolist
+        ''')
+        db.execute('DROP TABLE todolist')
+        db.execute('ALTER TABLE todolist_temp RENAME TO todolist')
 
 def main():
     db = create_connection()
@@ -110,8 +215,8 @@ def main():
     actions = {
         "1": add_task,
         "2": print_tasks,
-        "3": "Редактировать задачу",
-        "4": "Удалить задачу",
+        "3": edit_task,
+        "4": delete_tasks,
         #"5": "Закрыть приложение"
     }
 
@@ -121,7 +226,7 @@ def main():
         print("1. Добавить задачу")
         print("2. Посмотреть задачи")
         print("3. Редактировать задачу")
-        print("4. Удалить задачу из списка")
+        print("4. Удаление задач из списка")
         print("5. Закрыть приложение")
         print("-------------------")
         try:
